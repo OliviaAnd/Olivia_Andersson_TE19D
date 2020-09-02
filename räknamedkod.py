8f0838e8e513d188db662822f98f374e0c9f24e8
OliviaAnd <70136619+OliviaAnd@users.noreply.github.com>
2020-09-02 12:19:37 +0200

    Första lektionen

---
+++ räknamedkod.py
@@ -0,0 +1,37 @@
+#1
+print(12-7)
+print(24/3+9)
+print(3*43-2)
+
+#2
+print("Hej hur mår du?")
+
+#3
+Läsk = 15 
+print(2*Läsk)
+Hamburgare = 29 
+print(2*Läsk+Hamburgare)
+Pommes = 12
+print(3*Läsk+3*Hamburgare+3*Pommes)
+
+#4
+print(219*45+8)
+
+#5
+multi = (70*1.5)
+print(f"Om du kör med hastigheten 70 km/h i 1.5 timmar, så hinner du {multi} km")
+
+#6
+print(10*"Hej")
+
+#2.1 
+bredd = 5
+längd = 8 
+print("Omkretsen är", 2*(bredd+längd), "cm.")
+print("Arean är", längd*bredd, "kvadratcentimeter")
+
+bredd1 = 92 
+längd1 = 230 
+print("Omkretsen är", (2*bredd1+längd1), "cm.")
+print("Arean är", längd*bredd, "kvadratcentimeter.")
+
