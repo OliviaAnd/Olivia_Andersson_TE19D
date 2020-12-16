ef5cd25261968122561a8dbb6484ccad6d2495c2
OliviaAnd <70136619+OliviaAnd@users.noreply.github.com>
2020-12-16 11:50:14 +0100

    blackjack

--- Programmering1/Programmera mer/BlackJack/blackjack.py
+++ Programmering1/Programmera mer/BlackJack/blackjack.py
@@ -1,43 +1,68 @@
 import kortlek 
 
-lek = kortlek.skapaKortlek()
-
 print(lek)
 
 # behöver räkna ut värdet på korten  
+def räknaPoäng(hand):
+    poäng = 0 
+    for kort in hand:
+        if kort == "j" or kort == "Q" or kort == "K":
+            poäng += 10
+        elif kort == "A" and poäng < 11:
+            poäng += 11
+        elif kort == "A" and poäng >= 11:
+            poäng += 1
+        else:
+            poäng += kort
+    return poäng 
 
 # handen 
 def skrivUtHanden(hand):
     print("Dina kort är: ", end= "")
     for kort in hand:
-        print(str(kort)) #fortsätt här
+        print(str(kort) + ", ", end="") 
 
 # vinnaren 
+def checkaVinnare(hand, dealer):
+    dealerPoäng = räknaPoäng(dealer)
+    spelarePoäng = ränkaPoäng(hand)
+
+    if spelarePoäng == 21:
+        print("Blackjack")
+    elif spelarePoäng <= dealerPoäng or spelarePoäng > 21:
+        print("Huset vinner")
+    else:
+        print("Du vinner")
+
 
 # spel-loop 
-while True:
-    spela = input("Vill du spela Black Jack? (j för ja, annan tangent för nej)")
+    while True:
+        spela = input("Vill du spela Black Jack? (j för ja, annan tangent för nej)")
 
-    if spela != "j":
-        break
-    
-    lek = kortlek.skapaKortlek()
+        if spela != "j":
+            break
+        
+        lek = kortlek.skapaKortlek()
 
-    print(lek)
-    # dealer ger två kort 
-    dealer = [lek.pop(0),lek.pop(0)]
-    print(f"Dealerns första kort är {dealer[0]}")
-    
-    hand = {lek.pop(0), lek.pop(0)}
-    print(f"Dina första två kort är: {hand[0] och hand[1]}")
+        print(lek)
+        # dealer ger två kort 
+        dealer = [lek.pop(0),lek.pop(0)]
+        print(f"Dealerns första kort är {dealer[0]}")
+        
+        hand = {lek.pop(0), lek.pop(0)}
+        print(f"Dina första två kort är: {hand[0] och hand[1]}")
 
-    fortsätt = True
+        fortsätt = True
 
-# göra val (få fler kort, stanna)
-while fortsätt:
-    takort = input("Ta nytt kort? (j för ja, annan tangent för att stanna)")
+    # göra val (få fler kort, stanna)
+    while fortsätt:
+        takort = input("Ta nytt kort? (j för ja, annan tangent för att stanna)")
 
-    if takort == "j":
-        hand.append(lek.pop(0))
-    else:
-        fortsätt = False
+        if takort == "j":
+            hand.append(lek.pop(0))
+            skrivUtHanden(hand)
+        else:
+            fortsätt = False
+    print(f"dina poäng är {räknaPoäng(hand)}")
+
+print("Spelet slutar, ha de gött haj")
